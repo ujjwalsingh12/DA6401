a345e657e80d9d840dcd35eff3215a36fdf07b9d
Ujjwal Singh <51953621+ujjwalsingh12@users.noreply.github.com>
2025-04-20 23:13:57 +0530

    Update getdata.py

--- assignment2/getdata.py
+++ assignment2/getdata.py
@@ -7,14 +7,14 @@ import numpy as np
 from torchvision.datasets import ImageFolder
 from torch.utils.data import random_split
 
-# Give path to train folder
-train_data_path = "./inaturalist_12K/train"
+# Paths to dataset
+train_directory = "./inaturalist_12K/train"
 # Give path to val folder
-val_data_path = "./inaturalist_12K/val"
+val_directory= "./inaturalist_12K/val"
 
-# Transform the data
-data_transforms = {
-    'trainaugmentation': transforms.Compose([
+# Define transformation pipelines
+transform_pipelines = {
+    'augmented_train': transforms.Compose([
         transforms.Resize((256, 256)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(degrees=30),
@@ -22,22 +22,30 @@ data_transforms = {
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ]),
-    'val': transforms.Compose([
+    'standard_val': transforms.Compose([
         transforms.Resize((256, 256)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ]),
-    'train':transforms.Compose([
+    'standard_train': transforms.Compose([
         transforms.Resize((256, 256)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ]),
 }
-# Load the data and pass to te function
-def get_data (augmentation):
-    train_dataset = ImageFolder(root=train_data_path, transform=data_transforms['trainaugmentation'] if augmentation == 'yes' else data_transforms['train'])
-    validation_size = int(0.2 * len(train_dataset))
-    training_size = len(train_dataset) - validation_size
-    test_dataset = ImageFolder(root=val_data_path, transform=data_transforms['val'])
-    training_data, validation_data = random_split(train_dataset, [training_size, validation_size])
-    return training_data,validation_data,test_dataset
+
+# Function to load and split data
+def get_data(apply_augmentation):
+    full_train_dataset = ImageFolder(
+        root=train_directory,
+        transform=transform_pipelines['augmented_train'] if apply_augmentation == 'yes' else transform_pipelines['standard_train']
+    )
+
+    validation_split = int(0.2 * len(full_train_dataset))
+    training_split = len(full_train_dataset) - validation_split
+
+    training_dataset, validation_dataset = random_split(full_train_dataset, [training_split, validation_split])
+
+    test_dataset = ImageFolder(root=val_directory, transform=transform_pipelines['standard_val'])
+
+    return training_dataset, validation_dataset, test_dataset
